9a408aa609da68700bbaa85a1ff4cf642af00095
Lucas <silvalucas1912@gmail.com>
2022-12-13 15:18:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Price } from "./Components/Price";
+import "./global.css";
+
+function App() {
+  return <Price />;
+}
+
+export default App;
---
+++ src/Components/Price.tsx
@@ -0,0 +1,25 @@
+import "./Price.css";
+
+export function Price() {
+  return (
+    <>
+      <div className="geral">
+        <div className="cabecalho1">
+          <div className="cabecalho2">
+            <h1> Conheça nossos planos</h1>
+            <span>
+              Conheça nossas soluções e escolha o plano ideal para o seu próximo
+              projeto
+            </span>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="item1"></div>
+          <div className="item2"></div>
+          <div className="item3"></div>
+        </div>
+      </div>
+    </>
+  );
+}
