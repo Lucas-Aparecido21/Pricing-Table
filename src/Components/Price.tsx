eeb18c313257894d2f23067108ad200c07f39696
EquipeWeb02 <equipeweb02@Mac-Filipe.local>
2022-12-14 18:15:35 -0200

    14-12

--- src/Components/Price.tsx
+++ src/Components/Price.tsx
@@ -1,3 +1,4 @@
+import { Check } from "phosphor-react";
 import "./Price.css";
 
 export function Price() {
@@ -17,28 +18,94 @@ export function Price() {
         <div className="container">
           <div className="item1">
             <header className="planoGratisHeader">Grátis</header>
-            <h1 className="preco1">R$ 0</h1>
-            <span>,00 /mês</span>
-            <p className="conteudo1">
+
+            <div className="valor1">
+              <h1>R$ 0</h1>
+              <h1 className="decimais1">,00</h1>
+              <span className="preco1"> /mês</span>
+            </div>
+            <p className="informativo1">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
+
+            <div className="conteudo1">
+              <ul>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </div>
+            <footer className="footer1">
+              <button type="button" className="assinar1">
+                ASSINAR
+              </button>
+            </footer>
           </div>
+
           <div className="item2">
             <header className="planoBasicoHeader">Básico</header>
-            <h1 className="preco2">R$ 29</h1>
-            <span>,00 /mês</span>
-            <p className="conteudo2">
+            <div className="valor2">
+              <h1>R$ 29</h1>
+              <h1 className="decimais2">,00</h1>
+              <span className="preco2">/mês</span>
+            </div>
+            <p className="informativo2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
+            <div className="conteudo2">
+              <ul>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </div>
+
+            <footer className="footer2">
+              <button type="button" className="assinar2">
+                ASSINAR
+              </button>
+            </footer>
           </div>
           <div className="item3">
             <header className="planoProfissionalHeader">Profissional</header>
-            <h1 className="preco3">R$ 59</h1>
-            <span>,00 /mês</span>
-
-            <p className="conteudo3">
+            <div className="valor3">
+              <h1>R$ 59</h1>
+              <h1 className="decimais3">,00</h1>
+              <span className="preco3"> /mês</span>
+            </div>
+            <p className="informativo3">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
+            <div className="conteudo3">
+              <ul>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                  <Check /> Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </div>
+            <footer className="footer3">
+              <button type="button" className="assinar3">
+                ASSINAR
+              </button>
+            </footer>
           </div>
         </div>
       </div>
