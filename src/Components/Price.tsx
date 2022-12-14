d7fe650f0330c01c2074adc0abd2da73d81e1f4b
Lucas <silvalucas1912@gmail.com>
2022-12-14 15:16:20 -0300

    14-12-part1

--- src/Components/Price.tsx
+++ src/Components/Price.tsx
@@ -17,18 +17,28 @@ export function Price() {
         <div className="container">
           <div className="item1">
             <header className="planoGratisHeader">Grátis</header>
-            <h1>R$ 0</h1>
+            <h1 className="preco1">R$ 0</h1>
             <span>,00 /mês</span>
+            <p className="conteudo1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
           <div className="item2">
             <header className="planoBasicoHeader">Básico</header>
-            <h1>R$ 29</h1>
+            <h1 className="preco2">R$ 29</h1>
             <span>,00 /mês</span>
+            <p className="conteudo2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
           <div className="item3">
             <header className="planoProfissionalHeader">Profissional</header>
-            <h1>R$ 59</h1>
+            <h1 className="preco3">R$ 59</h1>
             <span>,00 /mês</span>
+
+            <p className="conteudo3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
         </div>
       </div>
